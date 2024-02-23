983baa94b284728036e5935a476a6428ee550ad2
Jake McIvor <jake.mcivor@gmail.com>
2024-02-23 16:18:39 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls.js';
+//import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls.js';
 import { STLLoader } from 'three/examples/jsm/loaders/STLLoader.js'
 
 const scene = new THREE.Scene()
